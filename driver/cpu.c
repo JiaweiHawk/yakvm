1457ee449ed1b075a8669282465393e12693923e
hawk <18801353760@163.com>
2024-03-20 13:06:36 +0800

    driver/cpu.c: introduce *struct vmcb*

--- driver/cpu.c
+++ driver/cpu.c
@@ -26,6 +26,7 @@ const struct file_operations yakvm_vcpu_fops = {
 struct vcpu *yakvm_create_vcpu(struct vm *vm)
 {
     struct vcpu *vcpu;
+    struct vmcb *vmcb;
 
     vcpu = kmalloc(sizeof(struct vcpu), GFP_KERNEL_ACCOUNT | __GFP_ZERO);
     if (!vcpu) {
@@ -33,7 +34,20 @@ struct vcpu *yakvm_create_vcpu(struct vm *vm)
         return ERR_PTR(-ENOMEM);
     }
 
+    /*
+     * Virtual Machine Control Block(vmcb) should be a
+     * 4KB-aligned page accroding to "2.2" on page 5 at
+     * https://www.0x04.net/doc/amd/33047.pdf
+     */
+    vmcb = kmalloc(4096, GFP_KERNEL_ACCOUNT | __GFP_ZERO);
+    if (!vmcb) {
+        log(LOG_ERR, "kmalloc() failed");
+        kfree(vcpu);
+        return ERR_PTR(-ENOMEM);
+    }
+
     /* initialize the vcpu */
+    vcpu->vmcb = vmcb;
     vcpu->vm = vm;
 
     return vcpu;
@@ -42,5 +56,6 @@ struct vcpu *yakvm_create_vcpu(struct vm *vm)
 /* destroy the vcpu */
 void yakvm_destroy_vcpu(struct vcpu *vcpu)
 {
+    kfree(vcpu->vmcb);
     kfree(vcpu);
 }
